b5e822f1a76fd327497bd3c8039777295a6e7707
Clément Voisin <clembs@clembs.com>
2025-01-29 17:35:45 +0100

    chore: ne pas afficher le bouton de suppression si le match est passé

--- src/views/matches/joueurs.php
+++ src/views/matches/joueurs.php
@@ -91,19 +91,21 @@ $estPassee = $rencontreSelectionnee->getDate() <= new DateTime() || $rencontreSe
           <div class="buttons">
             <button class="button primary inline" type="submit" form="<?= $formId ?>">Enregistrer</button>
 
-            <form action="/submit.php/?form=supprimer-participant&participant=<?= $participant->getId() ?>" method="POST">
-              <button type="submit" class="button danger inline icon" aria-label="Supprimer le participant">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                  class="lucide lucide-trash-2">
-                  <path d="M3 6h18" />
-                  <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-                  <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-                  <line x1="10" x2="10" y1="11" y2="17" />
-                  <line x1="14" x2="14" y1="11" y2="17" />
-                </svg>
-              </button>
-            </form>
+            <?php if (!$estPassee): ?>
+              <form action="/submit.php/?form=supprimer-participant&participant=<?= $participant->getId() ?>" method="POST">
+                <button type="submit" class="button danger inline icon" aria-label="Supprimer le participant">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-trash-2">
+                    <path d="M3 6h18" />
+                    <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                    <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                    <line x1="10" x2="10" y1="11" y2="17" />
+                    <line x1="14" x2="14" y1="11" y2="17" />
+                  </svg>
+                </button>
+              </form>
+            <?php endif; ?>
           </div>
         </td>
 
