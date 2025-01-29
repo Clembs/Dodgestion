d6e80ea834d34e444e8c15e3f9cb7713fcf24868
Clément Voisin <clembs@clembs.com>
2025-01-29 08:15:14 +0100

    feat: page 404 plus sympa

--- src/views/404.php
+++ src/views/404.php
@@ -2,14 +2,35 @@
 
 <?php ob_start(); ?>
 
-<h1>404</h1>
+<main>
+  <h1>404</h1>
 
-<p>Cette page n'a pas pu être trouvée.</p>
+  <p>Cette page n'a pas pu être trouvée.</p>
 
-<a href="/index.php">
-  Retour à l'accueil
-</a>
+  <a class="button primary" href="/">
+    Retour à l'accueil
+  </a>
+</main>
 
 <?php $content = ob_get_clean(); ?>
+
+<?php ob_start(); ?>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+  }
+
+  h1 {
+    font-size: 4rem;
+    font-weight: bold;
+    margin-bottom: 0;
+  }
+</style>
+
+<?php $head .= ob_get_clean(); ?>
 
 <?php require 'layout.php'; ?>
